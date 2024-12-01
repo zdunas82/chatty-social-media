6bbac531e5157cf2a95cbc51ffb2447e3666458c
zdunas82 <zdoonas82@gmail.com>
2024-12-01 22:26:16 +0000

    Fix Clerk publishable key issue

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you&apos;re looking for could not be found.</p>
+    </div>
+  );
+}
